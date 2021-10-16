bad4d246de67499eccaf5e9c9e3703c3284d08ef
ChristianM106 <christian.mora02@usc.edu.co>
2021-10-15 20:21:22 -0500

    SocketColas - Basico

---
+++ sockets/controller.js
@@ -0,0 +1,65 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
+const socketController = (socket) => {
+    
+    /* console.log('Cliente conectado', socket.id );
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id );
+    }); */
+
+    //Toso esto se ejecuta cuando un cliente se conecta a nuestro Servidor
+
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual', ticketControl.ultimos4);
+
+    socket.emit('ticket-pendientes', ticketControl.tickets.length);
+    
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguienteTicket();
+        callback(siguiente);
+        socket.broadcast.emit('ticket-pendientes', ticketControl.tickets.length);
+
+    });
+    
+    socket.on('atender-ticket',({escritorio},callback) =>{
+        
+        
+        if(!escritorio){
+            return callback({
+                ok:false,
+                msg:'El escritorio es obligatorio'
+            })
+        };
+        
+        const ticket = ticketControl.atenderTicket(escritorio);
+        
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+        socket.emit('ticket-pendientes', ticketControl.tickets.length);
+        socket.broadcast.emit('ticket-pendientes', ticketControl.tickets.length);
+
+        if(!ticket){
+            return callback({
+                ok:false,
+                msg:'Ya no hay tickets pendientes'
+            })
+        }else{
+            return callback({
+                ok:true,
+                ticket
+            })
+        };
+        
+    });
+
+}
+
+
+
+module.exports = {
+    socketController
+}
+
